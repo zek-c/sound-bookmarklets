efc8a4fbdf6e664ca70eabb06d1de1db3b432f4a
zek <105883248+zek-c@users.noreply.github.com>
2023-04-05 12:10:31 -0700

    Create auuughhhhh.js

---
+++ auuughhhhh.js
@@ -0,0 +1,1 @@
+javascript: (function() {  var audio = new Audio('https://github.com/3kh0/soundboard/blob/main/sounds/AUUGHHH.mp3?raw=true%27);  audio.addEventListener(%27canplay%27, () =>{  audio.play();  });})();
